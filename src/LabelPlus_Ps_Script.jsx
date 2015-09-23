52ee4e6d324c0f7f0e78c841f6f14bbac7de516b
noodlefighter <rongjiayu@gmail.com>
2015-09-24 01:49:27 +0800

    ps脚本 添加存储配置功能(未调试) 添加横版文字输出 增加文本替换功能(未调试)

--- src/LabelPlus_Ps_Script.jsx
+++ src/LabelPlus_Ps_Script.jsx
@@ -15,17 +15,28 @@ app;
 //
 
 // Gobal Const
-const _MY_APPNAME = "LabelPlus Input Text Tool";
+const _MY_APPNAME = "LabelPlus Inputer For FileVer<=";
 const _MY_VER = "1.0";
-const _MY_TIP_TITLE = "标题";
-const _MY_TIP_TEXT = "内容";
+const _MY_TIP_TITLE = "说明";
+const _MY_TIP_TEXT = "本脚本支持将LabelPlus格式的文本导入成ps图层。\r\n" +
+  "利用\“存取配置\”功能，可以方便的根据不同需求（如区分图片分辨率），快速应用配置。\r\n" +
+  "更多信息: http://noodlefighter.com/label_plus/";
+
+const _MY_STRING_BUTTON_RUN = "导入";
+const _MY_STRING_BUTTON_CANCEL = "关闭";
+const _MY_STRING_BUTTON_LOAD = "加载配置";
+const _MY_STRING_BUTTON_SAVE = "保存配置";
+const _MY_STRING_BUTTON_RESET = "还原配置";
 
 const _MT_STRING_LABEL_TEXTFILE = "LabelPlus文本:";
 const _MT_STRING_LABEL_SOURCE = "图源文件夹:";
 const _MT_STRING_LABEL_TARGET = "输出PSD文件夹:";
 const _MT_STRING_LABEL_FONT = "字体:";
+const _MT_STRING_LABEL_SETTING = "存取配置";
+const _MT_STRING_LABEL_SELECTIMAGE = "选择需要导入的图片";
 
 const _MT_STRING_CHECKBOX_OUTPUTLABELNUMBER = "导出标号";
+const _MT_STRING_CHECKBOX_TEXTREPLACE = "文本替换(例:\"A->B|C->D\")";
 const _MT_STRING_CHECKBOX_OUTPUTNOSIGNPSD = "处理无标号文档";
 const _MT_STRING_CHECKBOX_SETSOURCETYPE = "指定图源后缀名";
 const _MT_STRING_CHECKBOX_RUNACTION = "导入文本后，执行以分组名命名的动作";
@@ -33,7 +44,7 @@ const _MT_STRING_CHECKBOX_NOTCLOSE = "导入后不关闭文档";
 const _MT_STRING_CHECKBOX_SETFONT = "修改默认字体";
 const _MT_STRING_CHECKBOX_OUTPUTHORIZONTALTEXT = "输出横排文字";
 
-const _MT_STRING_LABEL_SELECTIMAGE = "选择需要导入的图片";
+const _MY_STRING_COMPLETE = "导出完毕！";
 
 const _MT_ERROR_NOTFOUNDSOURCE = "未找到图源文件夹";
 const _MT_ERROR_NOTFOUNDTARGET = "未找到目标文件夹";
@@ -41,6 +52,8 @@ const _MT_ERROR_NOTFOUNLABELTEXT = "未找到LabelPlus文本文件";
 const _MT_ERROR_CANNOTBUILDNEWFOLDER = "无法创建新文件夹";
 const _MT_ERROR_READLABELTEXTFILEFAILL = "解析LabelPlus文本失败";
 const _MT_ERROR_NOTCHOOSEIMAGE = "未选择输出图片";
+
+//todo: ini文件没法正常读写了
 
 //
 // 初始设置
@@ -63,23 +76,28 @@ LabelPlusInputOptions.LOG_FILE = Stdlib.PREFERENCES_FOLDER + "/LabelPlusInput.lo
 //
 LabelPlusInput = function() {
   var self = this;
-   
-  self.iniFile = LabelPlusInputOptions.INI_FILE;
-  self.saveIni = true;
-  self.optionsClass = LabelPlusInputOptions;
 
+  //self.iniFile = LabelPlusInputOptions.INI_FILE;
+  self.saveIni = false;
+  self.hasBorder = true;
+  self.optionsClass = LabelPlusInputOptions;
+  self.settingsPanel = false; //有自己创建的设置面板 不需要自带的
+  
   self.winRect = {          // the size of our window
     x: 200,
     y: 200,
     w: 675,
-    h: 490
+    h: 600
   };  
   
-  self.title = _MY_APPNAME + " " + _MY_VER;// our window title
+  self.title = _MY_APPNAME + _MY_VER;// our window title
   self.notesSize = 75;
   self.notesTxt = _MY_TIP_TITLE;
   self.documentation = _MY_TIP_TEXT;
-    
+  
+  self.processTxt = _MY_STRING_BUTTON_RUN;
+  self.cancelTxt = _MY_STRING_BUTTON_CANCEL;
+  
 };
 
 LabelPlusInput.prototype = new GenericUI();
@@ -90,22 +108,28 @@ LabelPlusInput.prototype.typename = "LabelPlusInput";
 //
 LabelPlusInput.prototype.createPanel = function(pnl, ini) {
   var self = this;
-  
-  var xOfs = 10;
-  var yy = 10;
-
   var opts = new LabelPlusInputOptions(ini);// default values
 
-  if (ini.uiX == undefined) {
-    ini.uiX = ini.uiY = 100;
-  }
-
   // window's location
-  self.moveWindow(100, 100);
-  
-  //------------------路径选择区------------------
-  var xx = xOfs;
+  self.moveWindow(100, 100);  
 
+  var xOfs = 10;
+  var yOfs = 10;
+  var xx = xOfs;
+  var yy = yOfs;  
+  
+  //------------------自己创建的配置面板------------------  
+  
+  pnl.settingsPnl = pnl.add('panel', 
+    [xOfs,yy,pnl.size.width-xOfs,yy+60]);   
+    
+  LabelPlusInput.createSettingsPanel(pnl.settingsPnl, ini);     
+  
+  xx = xOfs;
+  yy += 75;  
+  yOfs = yy; 
+  //------------------路径选择区------------------
+  
   // LabelPlus文本文件输入
   pnl.lpTextFileLabel = pnl.add('statictext', [xx,yy,xx+120,yy+20],
                             _MT_STRING_LABEL_TEXTFILE);
@@ -252,6 +276,20 @@ LabelPlusInput.prototype.createPanel = function(pnl, ini) {
   
   xx = xOfs;
   yy += 25;  
+
+  // 文本替换(例:"A->B|C->D")
+  pnl.textReplaceCheckBox = pnl.add('checkbox', [xx,yy,xx+250,yy+22],
+                                          _MT_STRING_CHECKBOX_TEXTREPLACE);
+  pnl.textReplaceCheckBox.onClick = function() {
+    pnl.textReplaceTextBox.enabled = pnl.textReplaceCheckBox.value;
+  }    
+  xx += 260;
+  pnl.textReplaceTextBox = pnl.add('edittext', [xx,yy,xx+180,yy+20]);  
+  pnl.textReplaceTextBox.text = "！？->!?|...->…";
+  pnl.textReplaceTextBox.enabled = false;
+
+  xx = xOfs;
+  yy += 25;
   
   // 使用自定义字体设置
   pnl.setFontCheckBox = pnl.add('checkbox', [xx,yy,xx+250,yy+22],
@@ -263,7 +301,7 @@ LabelPlusInput.prototype.createPanel = function(pnl, ini) {
     pnl.font.fontSize.enabled = value;
   }   
  
- xx = xOfs;
+  xx = xOfs;
   yy += 25;
   
   // 字体
@@ -320,10 +358,10 @@ LabelPlusInput.prototype.createPanel = function(pnl, ini) {
   pnl.notCloseCheckBox = pnl.add('checkbox', [xx,yy,xx+250,yy+22],
                                            _MT_STRING_CHECKBOX_NOTCLOSE );
   xx = xOfs;
-  yy += 25;
+  yy += 35;
 
   //------------------导入文件选择区------------------
-  yy = 10;
+  yy = yOfs;
   xOfs = 10 + 475;  
   xx = xOfs;  
 
@@ -331,64 +369,234 @@ LabelPlusInput.prototype.createPanel = function(pnl, ini) {
   pnl.chooseImageLabel =  pnl.add('statictext', [xx,yy,xx+150,yy+22],
                                            _MT_STRING_LABEL_SELECTIMAGE );
   yy += 20;
-  pnl.chooseImageListBox = pnl.add('listbox', [xx,yy,xx+150,yy+235], [] ,{multiselect:true});
+  pnl.chooseImageListBox = pnl.add('listbox', [xx,yy,xx+150,yy+285], [] ,{multiselect:true});
+
+  //------------------读取配置区------------------
+  if (ini) {   // if there was an ini object
+    //文本替换
+    if(ini.textReplace){
+      pnl.textReplaceCheckBox.value = true;
+      pnl.textReplaceTextBox.text = opts.textReplace;
+    }  
+    
+    // 字体  
+    if (ini.setFont) {
+      pnl.setFontCheckBox.value = true;
+      pnl.font.family.enabled =  true;
+      pnl.font.style.enabled =  true;
+      pnl.font.fontSize.enabled =  true;      
+      pnl.font.setFont(ini.font, ini.fontSize);
+    }
+    
+    // 导出标号选项
+    if(ini.outputLabelNumber){
+      pnl.outputLabelNumberCheckBox.value = ini.outputLabelNumber;
+    }
   
+    // 输出横排文字
+    if(ini.horizontalText){
+      pnl.outputHorizontalCheckBox.value = ini.horizontalText;
+    }
+    // 处理无标号文档
+    if(ini.outputNoSignPsd){
+      pnl.outputNoSignPsdCheckBox.value = ini.outputNoSignPsd;
+    }
+    // 使用指定类型图源
+    if (ini.sourceFileType){    
+      pnl.setSourceFileTypeCheckBox.value = true;
+      pnl.setSourceFileTypeList.enabled = true;
+      pnl.setSourceFileTypeList.selection.text = ini.sourceFileType;
+    }
+  
+    // 执行动作GroupN
+    if (ini.runActionGroup){
+      pnl.runActionGroupList.enabled = true;      
+      pnl.runActionGroupList.selection = ini.runActionGroup;  
+      pnl.runActionGroupCheckBox.value = true;
+    }  
+  
+    // 导入后不关闭文档
+    if(ini.notClose)
+      pnl.notCloseCheckBox.value = ini.notClose;
+      
+  } 
+
   return pnl;
+};
+
+//
+// 自定义读取配框
+//
+LabelPlusInput.createSettingsPanel = function(pnl, ini) {
+  var win = GenericUI.getWindow(pnl.parent);
+
+  pnl.text = _MT_STRING_LABEL_SETTING;
+  pnl.win = win;
+
+  pnl.fileMask = "INI Files: *.ini, All Files: *.*";
+  pnl.loadPrompt = "Read Setting";
+  pnl.savePrompt = "Save Setting";
+  pnl.defaultFile = undefined;
+
+  var w = pnl.bounds[2] - pnl.bounds[0];
+  var offsets = [w*0.2, w*0.5, w*0.8];
+  var y = 15;
+  var bw = 90;
+
+  var x = offsets[0]-(bw/2);
+  pnl.load = pnl.add('button', [x,y,x+bw,y+20], _MY_STRING_BUTTON_LOAD);
+  x = offsets[1]-(bw/2);
+  pnl.save = pnl.add('button', [x,y,x+bw,y+20], _MY_STRING_BUTTON_SAVE);
+  x = offsets[2]-(bw/2);
+  pnl.reset = pnl.add('button', [x,y,x+bw,y+20], _MY_STRING_BUTTON_RESET);
+
+  pnl.load.onClick = function() {
+    var pnl = this.parent;
+    var win = pnl.win;
+    var mgr = win.mgr;
+    var def = pnl.defaultFile;
+
+    if (!def) {
+      if (mgr.iniFile) {
+        def = GenericUI.iniFileToFile(mgr.iniFile);
+      } else {
+        def = GenericUI.iniFileToFile("~/settings.ini");
+      }
+    }
+
+    var f;
+    var prmpt = pnl.loadPrompt;
+    var sel = Stdlib.createFileSelect(pnl.fileMask);
+    if (isMac()) {
+      sel = undefined;
+    }
+    f = Stdlib.selectFileOpen(prmpt, sel, def);
+    if (f) {
+      win.ini = mgr.readIniFile(f);
+      win.close(4);
+
+      if (pnl.onLoad) {
+        pnl.onLoad(f);
+      }
+    }
+  };
+
+  pnl.save.onClick = function() {
+    var pnl = this.parent;
+    var win = pnl.win;
+    var mgr = win.mgr;
+    var def = pnl.defaultFile;
+
+    if (!def) {
+      if (mgr.iniFile) {
+        def = GenericUI.iniFileToFile(mgr.iniFile);
+      } else {
+        def = GenericUI.iniFileToFile("~/settings.ini");
+      }
+    }
+
+    var f;
+    var prmpt = pnl.savePrompt;
+    var sel = Stdlib.createFileSelect(pnl.fileMask);
+
+    if (isMac()) {
+      sel = undefined;
+    }
+    f = Stdlib.selectFileSave(prmpt, sel, def);
+
+    if (f) {
+      var mgr = win.mgr;
+      var res = mgr.validatePanel(win.appPnl, win.ini, true);
+
+      if (typeof(res) != 'boolean') {
+        mgr.writeIniFile(f, res);
+
+        if (pnl.onSave) {
+          pnl.onSave(f);
+        }
+      }
+    }
+  };
+
+  pnl.reset.onClick = function() {
+    var pnl = this.parent;
+    var win = pnl.win;
+    var mgr = win.mgr;
+
+    if (mgr.defaultIniFile) {
+      win.ini = mgr.readIniFile(mgr.defaultIniFile);
+
+    } else if (mgr.ini) {
+      win.ini = mgr.ini;
+    }
+
+    win.close(4);
+    if (pnl.onReset) {
+      pnl.onReset();
+    }
+  };
 };
 
 //
 // 读出用户UI数据
 //
-LabelPlusInput.prototype.validatePanel = function(pnl, ini) {
+LabelPlusInput.prototype.validatePanel = function(pnl, ini, tofile) {
   var self = this;
   var opts = new LabelPlusInputOptions(ini);
 
-  // 图源文件夹
-  if (pnl.sourceTextBox.text) {    
-    f = new Folder(pnl.sourceTextBox.text);
-  }
-  else{
-    return self.errorPrompt(_MT_ERROR_NOTFOUNDSOURCE);  
-  }
+  // 写配置项时无需存储这些
+  if(!tofile || tofile == false){
+    // 图源文件夹
+    if (pnl.sourceTextBox.text) {    
+      f = new Folder(pnl.sourceTextBox.text);
+    }
+    else{
+      return self.errorPrompt(_MT_ERROR_NOTFOUNDSOURCE);  
+    }
 
-  if (!f || !f.exists) {
-    return self.errorPrompt(_MT_ERROR_NOTFOUNDSOURCE);
-  }
-  opts.source = f.toUIString();
-  
-  // 输出目录
-  if (pnl.targetTextBox.text) {
-    f = new Folder(pnl.targetTextBox.text);
-    if (!f.exists) {
-      if (!f.create()) {
-        return self.errorPrompt(_MT_ERROR_CANNOTBUILDNEWFOLDER);
+    if (!f || !f.exists) {
+      return self.errorPrompt(_MT_ERROR_NOTFOUNDSOURCE);
+    }
+    opts.source = f.toUIString();
+    
+    // 输出目录
+    if (pnl.targetTextBox.text) {
+      f = new Folder(pnl.targetTextBox.text);
+      if (!f.exists) {
+        if (!f.create()) {
+          return self.errorPrompt(_MT_ERROR_CANNOTBUILDNEWFOLDER);
+        }
       }
     }
-  }
-  else{
-    return self.errorPrompt(_MT_ERROR_NOTFOUNDTARGET);
-  }
-  
-  
-  if (!f || !f.exists) {
-    return self.errorPrompt(_MT_ERROR_NOTFOUNDTARGET);
-  }
-  opts.target = f.toUIString();
+    else{
+      return self.errorPrompt(_MT_ERROR_NOTFOUNDTARGET);
+    }
+    
+    
+    if (!f || !f.exists) {
+      return self.errorPrompt(_MT_ERROR_NOTFOUNDTARGET);
+    }
+    opts.target = f.toUIString();
 
-  // LabelPlus文本
-  f = new File(pnl.lpTextFileTextBox.text);
-  if(!f || !f.exists) {
-    return self.errorPrompt(_MT_ERROR_NOTFOUNLABELTEXT);
+    // LabelPlus文本
+    f = new File(pnl.lpTextFileTextBox.text);
+    if(!f || !f.exists) {
+      return self.errorPrompt(_MT_ERROR_NOTFOUNLABELTEXT);
+    }
+    opts.labelFilename = pnl.lpTextFileTextBox.text;
+    
+    // Image选择  
+    if(!pnl.chooseImageListBox.selection || pnl.chooseImageListBox.selection.length == 0)
+      return self.errorPrompt(_MT_ERROR_NOTCHOOSEIMAGE);
+    else
+      opts.imageSelected = pnl.chooseImageListBox.selection;  
   }
-  opts.labelFilename = pnl.lpTextFileTextBox.text;
   
-  // Image选择  
-  if(!pnl.chooseImageListBox.selection || pnl.chooseImageListBox.selection.length == 0)
-    return self.errorPrompt(_MT_ERROR_NOTCHOOSEIMAGE);
-  else
-    opts.imageSelected = pnl.chooseImageListBox.selection;  
-  
-  
+  // 文本替换  
+  if(pnl.textReplaceCheckBox){
+    opts.textReplace = pnl.textReplaceTextBox.text;
+  }
+
   // 字体  
   opts.setFont = pnl.setFontCheckBox.value;
   var font = pnl.font.getFont()
@@ -437,6 +645,10 @@ LabelPlusInput.prototype.process = function(opts, doc) {
   //解析LabelPlus文本
   var lpFile = new LabelPlusTextReader(opts.labelFilename);
   
+  //读取文本替换配置
+  if(opts.textReplace)
+    var textReplace = LabelPlusInput.textReplaceReader(opts.textReplace);
+  
   //选项排序
   opts.imageSelected.sort();
   
@@ -445,7 +657,7 @@ LabelPlusInput.prototype.process = function(opts, doc) {
     var originName = opts.imageSelected[i].text;
     var filename;
     var labelData = lpFile.LabelData[originName];
-    var gourpData = lpFile.GroupData;
+    var gourpData = lpFile.GroupData;    
     
     // 根据sourceFileType替换文件后缀名      
     if(opts.sourceFileType){
@@ -458,6 +670,7 @@ LabelPlusInput.prototype.process = function(opts, doc) {
     if(!opts.outputNoSignPsd && labelData.length == 0)
       continue;
       
+    // 打开图片文件
     var bgFile = File(opts.source + "//" + filename);
     if(!bgFile || !bgFile.exists){
       var msg = "Image " + filename + " Not Found.";
@@ -466,7 +679,7 @@ LabelPlusInput.prototype.process = function(opts, doc) {
       continue;
     } 
       
-    // 打开文件 
+    // 在PS中打开文件 
     var bg = app.open(bgFile);
     
     var layerGroups = new Array();
@@ -498,8 +711,14 @@ LabelPlusInput.prototype.process = function(opts, doc) {
             90);
         }
       
+        // 替换文本
+        if(textReplace){
+          for(var k=0;k<textReplace.length;k++)
+            labelString = labelString.replace (textReplace[k].From, textReplace[k].To);
+        }
+      
         // 导出文本
-        if(labelString != ""){
+        if(labelString && labelString != ""){
           artLayer = LabelPlusInput.newTextLayer(bg,
             labelString,
             labelX,
@@ -530,7 +749,7 @@ LabelPlusInput.prototype.process = function(opts, doc) {
     if(!opts.notClose)
       bg.close();    
   }
-  
+  alert(_MY_STRING_COMPLETE);
   Stdlib.log("Complete!");
 };
 
@@ -686,6 +905,7 @@ var blocks = str.split ("-");
     };
 };
 
+//创建文本图层
 LabelPlusInput.newTextLayer = function(doc,text,x,y,font,size,isVertical,opacity,group) {
   artLayerRef = doc.artLayers.add();
   artLayerRef.kind = LayerKind.TEXT;
@@ -708,6 +928,33 @@ LabelPlusInput.newTextLayer = function(doc,text,x,y,font,size,isVertical,opacity
     artLayerRef.move(group, ElementPlacement.PLACEATBEGINNING);  
     
   return artLayerRef;
+}
+
+// 文本替换字符串解析程序
+LabelPlusInput.textReplaceReader = function(str){
+  var arr = new Array();
+  var strs = str.split('|');
+  if(!strs)
+    return; //解析失败
+    
+  for(var i=0; i<strs.length; i++){
+    if(!strs[i] || strs[i]=="")
+      continue;
+      
+    var strss = strs[i].split("->");
+    if((strss.length != 2) || (strss[0]=="") || (strss[1]==""))
+      return; //解析失败
+    
+    arr.push({
+      From : strss[0],
+      To : strss[1],
+    });
+  }
+
+  if(arr.length != 0)
+    return arr;
+  else 
+    return;
 }
 
 // 主程序
