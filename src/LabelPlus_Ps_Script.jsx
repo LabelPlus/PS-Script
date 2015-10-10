26d909a6f13f7318d23eaa5fc67fade7fc8e7451
noodlefighter <rongjiayu@gmail.com>
2015-10-10 18:07:27 +0800

    ps脚本加入GRL注释

--- src/LabelPlus_Ps_Script.jsx
+++ src/LabelPlus_Ps_Script.jsx
@@ -1,6 +1,11 @@
 //
 //   LabelPlus_Ps_Script.jsx
 //   This is a Input Text Tool for LabelPlus Text File.
+// 
+// Copyright 2015, Noodlefighter
+// Released under GPL License.
+//
+// License: http://noodlefighter.com/label_plus/license
 //
 //@show include
 //
