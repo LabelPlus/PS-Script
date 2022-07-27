1f3aa07cffd8b3b522ef5db7571eb08f1a9dfb21
noodlefighter <noodlefighter@gmail.com>
2022-07-27 22:29:14 +0800

    fix #105, crash if open a custom option includes a font that doesn't exist

--- src/main.ts
+++ src/main.ts
@@ -611,7 +611,8 @@ class LabelPlusInput extends GenericUI {
                 pnl.setFontCheckBox.value = false;
             } else {
                 pnl.setFontCheckBox.value = true;
-                pnl.font.setFont(opts.font, opts.fontSize);
+                pnl.font.family.selection = pnl.font.family.find(opts.font);
+                pnl.font.fontSize.text    = opts.fontSize;
             }
             Emit(pnl.setFontCheckBox.onClick);
         }
